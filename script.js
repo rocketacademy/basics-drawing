5933227bf317d5ef764f6faa6aae172e636700de
alvinljt <alvinljt@gmail.com>
2020-08-28 17:54:48 +0800

    number of characters

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Please declare functions and variables above where they are used.
 
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var counter = 0;
+  var myOutputValue = '';
+  while (counter < input) {
+    myOutputValue += '😊';
+    counter += 1;
+  }
   return myOutputValue;
 };
