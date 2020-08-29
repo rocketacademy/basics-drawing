a32c6e40deee2a78b25e03010a355b249ebbfecf
alvinljt <alvinljt@gmail.com>
2020-08-29 10:26:27 +0800

    upside down triangle

--- script.js
+++ script.js
@@ -1,52 +1,19 @@
-// Initial mode is shape entering mode
-var mode = 'shape entering mode';
-// shapeMode to record the current shape the game is using
-var shapeMode;
-
 var main = function (input) {
   var rowCounter = 0;
-  var columnCounter;
+  var columnCounter = input;
   var numOfColumns;
   var myOutputValue = '';
 
-  // changing the mode according to the shape user inputs want
-  if (mode == 'shape entering mode') {
-    if (input == 'square') {
-      shapeMode = 'square';
-      mode = 'shape mode';
-      myOutputValue = 'please enter number of dimensions';
-    } else if (input == 'triangle') {
-      shapeMode = 'triangle';
-      mode = 'shape mode';
-      myOutputValue = 'please enter number of dimensions';
-    } else {
-      myOutputValue = 'you have entered an invalid input. Please enter square or triangle.';
+  // game logic for upside down triangle
+  while (rowCounter < input) {
+    numOfColumns = columnCounter;
+    while (numOfColumns > 0) {
+      myOutputValue += '😀';
+      numOfColumns -= 1;
     }
-  } else if (mode == 'shape mode') {
-    if (shapeMode == 'square') {
-      // game logic for square shapeMode
-      while (rowCounter < input) {
-        columnCounter = 0;
-        while (columnCounter < input) {
-          myOutputValue += '😀';
-          columnCounter += 1;
-        }
-        myOutputValue += '<br>';
-        rowCounter += 1;
-      }
-    } else if (shapeMode == 'triangle') {
-      // game logic for triangle shapeMode
-      while (rowCounter < input) {
-        columnCounter = 0;
-        numOfColumns = rowCounter + 1;
-        while (columnCounter < numOfColumns) {
-          myOutputValue += '😀';
-          columnCounter += 1;
-        }
-        myOutputValue += '<br>';
-        rowCounter += 1;
-      }
-    }
+    myOutputValue += '<br>';
+    rowCounter += 1;
+    columnCounter -= 1;
   }
   return myOutputValue;
 };
