e0c73eec5e66e42fc9b15951a39a7cbb96061a66
sophiaratna <sophia.ratna92@gmail.com>
2020-08-29 09:17:40 +0800

    Complete project till Centre Square

--- script.js
+++ script.js
@@ -1,6 +1,132 @@
-// Please declare functions and variables above where they are used.
+var myOutputValue = ""; 
+var gameStage = "first";
+var counter = 0;
+var dimension ="";
+var shapeCreated = "";
 
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  if (gameStage=="first"){
+    if (input =="square" || input == "triangle" || input == "upside down triangle" || input == "outline square" || input == "centre square"){
+      shapeCreated = input;
+      myOutputValue = "Hello! You have requested to create " + shapeCreated + " shape! Please enter the dimension now! " + "<br>";
+      gameStage = "second";
+    }  
+    else {
+      myOutputValue = 'Please enter either "square" or "triangle" or "upside down triangle" or "outline square" or "centre square"';
+    }
+   console.log("Shape: "+ shapeCreated) ;
+  }  
+  else if (gameStage=="second"){
+    if (shapeCreated == "square"){
+      dimension = input;
+      console.log ("gamestage: " + gameStage + ". And dimension: " + dimension)
+      while (counter < dimension) {
+        var innerCounter = 0;  
+        while (innerCounter < dimension){
+          myOutputValue = myOutputValue + " 😆 ";
+          innerCounter = innerCounter + 1;
+        }
+        counter = counter + 1;  
+        myOutputValue = myOutputValue + "<br>";
+      }
+    }
+    else if (shapeCreated == "triangle"){
+      dimension = input;
+      while (counter < dimension){
+        var innerCounter = -1;  
+        while (innerCounter < counter){
+          myOutputValue = myOutputValue + " 😆 ";
+          innerCounter = innerCounter + 1 ;
+        }
+        counter = counter + 1; 
+        myOutputValue = myOutputValue + "<br>";
+      }
+    }
+    else if (shapeCreated == "upside down triangle"){
+      counter = input;
+      dimension = input;
+      while (counter != 0) {
+        var innerCounter = 0;
+        while (innerCounter<counter){
+          myOutputValue = myOutputValue + " 😆 ";
+          innerCounter = innerCounter + 1; 
+        } 
+        myOutputValue = myOutputValue + "<br>"
+        counter = counter - 1;
+      }
+    }
+    else if (shapeCreated == "outline square"){
+      while (counter < input){
+        if (counter == 0 || counter == (input-1)){
+          var innerCounter = 0;
+          while (innerCounter < input){
+            myOutputValue = myOutputValue + " 😆 ";
+            innerCounter = innerCounter + 1;
+          }
+        }
+        
+        else if (counter != 0 || counter != (input-1)){
+          var innerCounter = 0;
+          while (innerCounter < input){
+            if (innerCounter == 0 || innerCounter == (input-1)){
+              myOutputValue = myOutputValue + " 😆 ";
+            }
+            else{
+              myOutputValue = myOutputValue + " 🥳 ";
+            }
+            innerCounter = innerCounter + 1;
+          }
+        }
+        counter = counter + 1;
+        myOutputValue = myOutputValue + "<br>";
+      } 
+    }
+    else if (shapeCreated == "centre square"){
+      if (input%2 != 1){
+        myOutputValue = "Please enter only odd-number dimension" + "<br>";
+      }
+      else {
+        while (counter < input){
+          if (counter == 0 || counter == (input-1)){
+            var innerCounter = 0;
+            while (innerCounter < input){
+              myOutputValue = myOutputValue + " 😆 ";
+              innerCounter = innerCounter + 1;
+            }
+          }
+          else if (counter == (input/2 - 0.5)){
+            var innerCounter = 0;
+            while (innerCounter < input){
+              if (innerCounter == 0 || innerCounter == (input-1)){
+                myOutputValue = myOutputValue + " 😆 ";
+              }
+              else if (innerCounter == (input/2 - 0.5)){
+                myOutputValue = myOutputValue + " 😈 ";
+              }
+              else {
+                myOutputValue = myOutputValue + " 🥳 ";
+              }
+              innerCounter = innerCounter + 1;
+            }
+          }
+    
+          else {
+            var innerCounter = 0;
+            while (innerCounter < input){
+              if (innerCounter == 0 || innerCounter == (input-1)){
+                myOutputValue = myOutputValue + " 😆 ";
+              }
+              else{
+                myOutputValue = myOutputValue + " 🥳 ";
+              }
+              innerCounter = innerCounter + 1;
+            }
+          }
+          counter = counter + 1;
+          myOutputValue = myOutputValue + "<br>";
+        }
+      }
+    }
+  }    
   return myOutputValue;
-};
+}
