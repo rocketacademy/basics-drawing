3c8b3ce8816e1a44aaacca4e0e3fe7191d49b76c
pinnlawjindakul <pachara.lawjindakul@gmail.com>
2020-08-29 11:18:18 +0800

    square mode based on dimension

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // Please declare functions and variables above where they are used.
 
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
+  var lineCounter = 0
+  while (lineCounter < input) {
+    var columnCounter = 0;
+    while (columnCounter < input) {
+      myOutputValue = myOutputValue + "😀";
+      columnCounter = columnCounter + 1;
+    }
+    myOutputValue = myOutputValue + '<br>';
+    lineCounter = lineCounter + 1;
+  }
   return myOutputValue;
-};
+}
