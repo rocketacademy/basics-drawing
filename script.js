eba571e12aa6e832d6f5d1e4c3004106415229bf
cynthcxy <cynthcxy@gmail.com>
2020-09-28 22:21:47 +0800

    add drawing exercise up to triangle rules

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = '';
+
+  var rowCounter = 0;
+
+  while (rowCounter < input) {
+
+    var columnCounter = 0;
+
+    while (columnCounter < rowCounter + 1) {
+      myOutputValue = myOutputValue + '🍔';
+      columnCounter = columnCounter + 1;
+    }
+    myOutputValue = myOutputValue + '<br>';
+    rowCounter = rowCounter + 1;
+  }
+
   return myOutputValue;
 };
+
