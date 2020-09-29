72eb7b3a6ff6709e76746fc9532c75f37b37baca
your-github-username <github-acct-email@example.com>
2020-09-29 18:16:53 +0800

    Haruka's homework for drawing untill 4 thumsup

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 var main = function (input) {
   var myOutputValue = '';
-
   var counter = 0;
-
   while (counter < input) {
     myOutputValue = myOutputValue + '👍';
-
     counter = counter + 1;
   }
-
   return myOutputValue;
 };
