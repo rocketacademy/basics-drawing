5051f0115cfd1b24e20293dcb76f712dd53d8ef8
jeremylim91 <jeremylim_91@live.com>
2020-09-30 15:40:46 +0800

    Changed to thumbs up emoji

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var main = function (input) {
     console.log('start inner counter');
     var innerCounter = 0;
     while (innerCounter < counter + 1) {
-      myOutputValue = myOutputValue + 'Y';
+      myOutputValue = myOutputValue + '👍';
       innerCounter = innerCounter + 1;
     }
     myOutputValue = myOutputValue + '<br>'
