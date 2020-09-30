ae679f0dc0a86490f15bc4b6017879b23f61d643
jeremylim91 <jeremylim_91@live.com>
2020-09-30 15:50:11 +0800

    added alternting emojis

--- script.js
+++ script.js
@@ -9,9 +9,12 @@ var main = function (input) {
     console.log('start inner counter');
     var innerCounter = 0;
     while (innerCounter < counter + 1) {
-      myOutputValue = myOutputValue + '👍';
-      innerCounter = innerCounter + 1;
+      if (innerCounter % 2 == 0) {
+        myOutputValue = myOutputValue + '👍';
+      } else myOutputValue = myOutputValue + '✊'
+      innerCounter = innerCounter + 1
     }
+
     myOutputValue = myOutputValue + '<br>'
     counter = counter + 1;
   }
