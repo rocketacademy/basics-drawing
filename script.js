cc729c19a7efe7f60bb1ae0e3bd78372e29f38ce
jeremylim91 <jeremylim_91@live.com>
2020-09-30 15:08:34 +0800

    basic version of drawing square

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var counter = 0;
+  var myOutputValue = ''
+  console.log('default \'myoutputvalue\'')
+  console.log('start outer counter');
+
+
+  while (counter < input) {
+    console.log('start inner counter');
+    var innerCounter = 0;
+    while (innerCounter < input) {
+      myOutputValue = myOutputValue + '(Y)';
+      innerCounter = innerCounter + 1;
+    }
+    myOutputValue = myOutputValue + '<br>'
+    counter = counter + 1;
+  }
   return myOutputValue;
 };
