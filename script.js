a76cdf866caefe49ef6f130115f8a9eed7559e9e
your-github-username <github-acct-email@example.cgit config --global user.name your-github-usernamegit config --global user.email github-acct-emai>
2020-10-02 14:23:17 +0700

    Adeline's Emojis up to triangle

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
+// // No of Emojis
+// var main = function (input) {
+//   var noOfEmojis = Number(input);
+//   var myOutputValue = '';
+//   var counter = 0
+//   while (counter < input) {
+//     counter += 1;
+//     myOutputValue += '👍';
+//   }
+//   return myOutputValue
+// }
+
+// // Square
+// var main = function (input) {
+//   var myOutputValue = '';
+//   var sideLength = Number(input);
+//   var columnCounter = 0;
+//   while (columnCounter < sideLength) {
+//     var rowCounter = 0;
+//     while (rowCounter < sideLength) {
+//       rowCounter += 1;
+//       myOutputValue += '👍';
+//     }
+//     myOutputValue += '<br>';
+//     columnCounter += 1;
+//   }
+//   return myOutputValue;
+// };
+
+// Triagle
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
-};
+  var numberOfRows = Number(input);
+  var counter = 0;
+  var myOutputValue = '';
+  while (counter < numberOfRows) {
+    var innerCounter = 0;
+    while (innerCounter <= counter) {
+      innerCounter += 1
+      myOutputValue += '👍';
+    }
+    counter += 1;
+    myOutputValue += '<br>'
+  }
+  return myOutputValue
+}
