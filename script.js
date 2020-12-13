9d47569d7dab5a9b453abee5b31df2402a544d32
iannyip <ianyip@nullspace.com.sg>
2020-12-13 22:47:38 +0800

    modified for outline

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ var main = function (input) {
   var counter  = 0;
   while (counter < input) {
     var innerCounter = 0;
-    while (innerCounter <= counter){
-      myOutputValue = myOutputValue + '💩';
+    while (innerCounter < input){
+      if ((counter > 0 && counter < input - 1) && (innerCounter > 0 && innerCounter < input -1)){
+        myOutputValue = myOutputValue + '🚽';
+      }
+      else{
+        myOutputValue = myOutputValue + '💩';
+      }
       innerCounter = innerCounter + 1;
     }
     myOutputValue = myOutputValue + '<br>';
