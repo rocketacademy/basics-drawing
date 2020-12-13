74a3b96181b551d0f44dac32e1818607228b48b0
iannyip <ianyip@nullspace.com.sg>
2020-12-13 22:27:19 +0800

    Submitting square

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = '';
+
+  var counter  = 0;
+  while (counter < input) {
+    var innerCounter = 0;
+    while (innerCounter < input){
+      myOutputValue = myOutputValue + '💩';
+      innerCounter = innerCounter + 1;
+    }
+    myOutputValue = myOutputValue + '<br>';
+    counter = counter + 1;
+  }
+
   return myOutputValue;
 };
