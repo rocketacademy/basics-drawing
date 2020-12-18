1f510453c60068e8bf189577b68637cf409c4fec
Jerome CHUA <jeromechua@google.com>
2020-12-18 08:48:52 +0800

    Complete Let's Draw V1 (Number of Characters)

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+// ----------------------------------------
+// Let's Draw V1 (Number of Characters)
+// ----------------------------------------
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "Let's draw: <br>";
+
+  // Continue to add 👍 to myOutputValue as long as `i` is not more than the input number
+  for (var i = 0; i < input; i += 1) {
+    myOutputValue += '👍';
+  }
+
   return myOutputValue;
 };
